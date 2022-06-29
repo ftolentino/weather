aed6d611a060dab1104e6eda2f05ac18e276b6cd
ftolentino <filmer.tolentino@gmail.com>
2022-06-28 20:50:32 -0700

    async and await lesson

--- src/index.js
+++ src/index.js
@@ -1,42 +1,68 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import WeatherService from './js/weather-service.js'
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import WeatherService from "./js/weather-service.js";
 
 function clearFields() {
-  $('#location').val("");
-  $('.showHumidity').text("");
-  $('.showTemp').text("");
-  $('.showFeels').text("");
-  $('.showPressure').text("");
-  $('.showMax').text("");
-  $('.showMin').text("");
-  $('.showDesc').text("");
+  $("#location").val("");
+  $(".showHumidity").text("");
+  $(".showTemp").text("");
+  $(".showFeels").text("");
+  $(".showPressure").text("");
+  $(".showMax").text("");
+  $(".showMin").text("");
+  $(".showDesc").text("");
 }
 
+function getElements(response) {
+  if (response.main) {
+    $(".showHumidity").text(
+      `The humidity in ${response.name} is ${response.main.humidity}%`
+    );
+    $(".showTemp").text(
+      `The temperature in Fahrenheit is ${toFahrenheit(
+        response.main.temp
+      )} degrees.`
+    );
+    $(".showFeels").text(
+      `The temperature in Fahrenheit feels like ${toFahrenheit(
+        response.main.feels_like
+      )} degrees.`
+    );
+    $(".showPressure").text(
+      `The current pressure is ${response.main.pressure}hPa.`
+    );
+    $(".showMax").text(
+      `The max temperature for today in Fahrenheit is ${toFahrenheit(
+        response.main.temp_max
+      )} degrees.`
+    );
+    $(".showMin").text(
+      `The minimum temperature for today in Fahrenheit is ${toFahrenheit(
+        response.main.temp_min
+      )} degrees.`
+    );
+    $(".showDesc").text(
+      `The forecast today includes ${response.weather[0].main} skies.`
+    );
+  }
+
+  function toFahrenheit(temp) {
+    let newTemp = parseFloat(((temp - 273.15) * 9) / 5 + 32).toFixed(2);
+    return newTemp;
+  }
+}
+
+async function makeApiCall(city) {
+  const response = await WeatherService.getWeather(city);
+  getElements(response);
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    let location = $('#location').val();
+    let city = $('#location').val();
     clearFields();
-    let promise = WeatherService.getWeather(location);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${location} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Fahrenheit is ${toFahrenheit(body.main.temp)} degrees.`);
-      $('.showFeels').text(`The temperature in Fahrenheit feels like ${toFahrenheit(body.main.feels_like)} degrees.`);
-      $('.showPressure').text(`The current pressure is ${body.main.pressure}hPa.`);
-      $('.showMax').text(`The max temperature for today in Fahrenheit is ${toFahrenheit(body.main.temp_max)} degrees.`);
-      $('.showMin').text(`The minimum temperature for today in Fahrenheit is ${toFahrenheit(body.main.temp_min)} degrees.`);
-      $('.showDesc').text(`The forecast today includes ${body.weather[0].main} skies.`);
-    }, function(error) {
-      $('.showError').text(`There was an error processing your request; ${error}`)
-    });    
-    
-    function toFahrenheit(temp) {
-      let newTemp = parseFloat(((temp) - 273.15) * 9/5 + 32).toFixed(2);
-      return newTemp;
-    }
+    makeApiCall(city);
   });
 });
--- src/js/weather-service.js
+++ src/js/weather-service.js
@@ -1,18 +1,14 @@
 //business logic
 export default class WeatherService {
-  static getWeather(location) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-        reject(request.response);
-        }
+  static async getWeather(location) {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
       }
-      request.open("GET", url, true);
-      request.send;
-    });
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
